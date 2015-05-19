49f7f0e55ed93c26457ab1b17d431e5013bdf38d
Robert Chavez <robertchavez@saasmath.seattleacademy.org>
2015-05-19 14:03:12 -0700

    add index.js geometry module and app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var geometry = require('./index.js');
+console.log('The area of a circle of radius 4 is '
+           + geometry.circlearea(4));
+
+console.log('The circumference of a circle of radius 4 is',geometry.circlecircumference(4));
+
+console.log('The area of a square with a width of 4 is',geometry.squarearea(4));
